e339b05fc7891d4cb3246f196878227c14356687
poyu <chen06115@gmail.com>
2024-11-08 18:16:19 +0800

    創建web初始檔案

---
+++ web/senior_project_web/senior_project_web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace senior_project_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
