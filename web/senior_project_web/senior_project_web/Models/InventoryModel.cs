a4037b7a7e3786d15cac785bf77728281ae4245c
poyu <chen06115@gmail.com>
2024-11-14 00:48:39 +0800

    新增庫存Model

---
+++ web/senior_project_web/senior_project_web/Models/InventoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senior_project_web.Models
+{
+    public class InventoryModel
+    {
+        [Key]
+        public string inventory_id { get; set; } //Guid
+
+        [Required]
+        public int quantity { get; set; }
+
+        public DateTime create_at { get; set; } = DateTime.Now;
+        public DateTime update_at { get; set; } = DateTime.Now;
+    }
+}
---
+++ web/senior_project_web/senior_project_web/Models/MealModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senior_project_web.Models
+{
+    public class MealModel
+    {
+        [Key]
+        public string meal_id {  get; set; } //Guid
+
+        [Required]
+        [MaxLength(255)]
+        public string name { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string type { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string img_path { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string description { get; set; }
+
+        [Required]
+        public int price { get; set; }
+
+        [Required]
+        public int cost { get; set; }
+
+        //FK
+        [Required]
+        public string inventory_id { get; set; }
+        public InventoryModel inventory { get; set; }
+    }
+}
