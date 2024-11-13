5dbdfd1a06e9ca2e71ed6112d190c218a90977ec
poyu <chen06115@gmail.com>
2024-11-14 00:48:14 +0800

    新增AdminModel並完成Admin登入註冊登出功能

---
+++ web/senior_project_web/senior_project_web/Models/AdminModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace senior_project_web.Models
+{
+    public class AdminModel
+    {
+        [Key]
+        public Guid admin_id { get; set; }    //使用Guid生成
+
+        public int admin_account {  get; set; } //自動遞增
+
+        [Required]
+        [MaxLength(255)]
+        public string password { get; set; }
+
+        public DateTime create_at { get; set; } = DateTime.Now;
+        public DateTime update_at { get; set;} = DateTime.Now;
+
+        //FK
+        [Required]
+        [ForeignKey("user_id")]
+        public string user_id { get; set; }
+        public UserModel User { get; set; }
+    }
+}
