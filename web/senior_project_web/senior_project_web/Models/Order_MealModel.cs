5532d45fa96374971873f2fff7aa2ab257987dda
poyu <chen06115@gmail.com>
2024-11-14 00:59:29 +0800

    新增Order_MealModel

---
+++ web/senior_project_web/senior_project_web/Models/Order_MealModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senior_project_web.Models
+{
+    public class Order_MealModel
+    {
+        [Key]
+        public int order_meal_id {  get; set; }
+    
+        public int quantity { get; set; }
+
+        //FK
+        [Required]
+        public int order_id { get; set; }
+        public OrderModel order { get; set; }
+
+        //FK
+        [Required]
+        public string meal_id { get; set; }
+        public MealModel meal { get; set; }
+    }
+}
