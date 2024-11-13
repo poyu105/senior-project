2e3a13ea952eadffa91112cc8a0fd9e2130854f5
poyu <chen06115@gmail.com>
2024-11-14 00:42:25 +0800

    註冊資料庫和Cookie驗證

--- web/senior_project_web/senior_project_web/Program.cs
+++ web/senior_project_web/senior_project_web/Program.cs
@@ -1,4 +1,25 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using senior_project_web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//註冊資料庫
+builder.Services.AddDbContext<OrderSystemDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OrderSystem"));
+});
+
+//全域範圍驗證機制
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+    //未登入時自動轉移至此網址
+    //option.LoginPath = new PathString("/Home/Login");
+    //未授權角色時會轉移至此網址
+    //option.AccessDeniedPath = new PathString("/Home/NoRole");
+    //登入後10分鐘後失效
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -18,6 +39,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//登入驗證機制(順序不能顛倒)
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
