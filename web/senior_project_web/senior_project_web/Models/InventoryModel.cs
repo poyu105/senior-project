8021c86225eb1ea2f2a70e57a7015a43cc5f39ab
poyu <chen06115@gmail.com>
2024-11-14 21:52:09 +0800

    新增庫存Model的外鍵連接

--- web/senior_project_web/senior_project_web/Models/InventoryModel.cs
+++ web/senior_project_web/senior_project_web/Models/InventoryModel.cs
@@ -5,12 +5,14 @@ namespace senior_project_web.Models
     public class InventoryModel
     {
         [Key]
-        public string inventory_id { get; set; } //Guid
+        public Guid inventory_id { get; set; } //Guid
 
         [Required]
         public int quantity { get; set; }
 
         public DateTime create_at { get; set; } = DateTime.Now;
         public DateTime update_at { get; set; } = DateTime.Now;
+
+        public ICollection<MealModel> Meal { get; set; }
     }
 }
