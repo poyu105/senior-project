1494d6bc2c539412439e9786535c574d72e2e4be
poyu <chen06115@gmail.com>
2024-11-21 19:25:17 +0800

    新增資料庫連線、返回菜單至index，回傳MealInfo至前端modal的API功能

--- web/senior_project_web/senior_project_web/Controllers/HomeController.cs
+++ web/senior_project_web/senior_project_web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using senior_project_web.Data;
 using senior_project_web.Models;
 using System.Diagnostics;
 
@@ -7,15 +9,30 @@ namespace senior_project_web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly OrderSystemDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, OrderSystemDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var meals = _context.Meal.ToList();
+            return View(meals);
+        }
+
+        [HttpGet]
+        [Route("meals/{meal_id}")]
+        public async Task<IActionResult> GetMealInfo(Guid meal_id)
+        {
+            var meal = await _context.Meal.FirstOrDefaultAsync(m => m.meal_id == meal_id);
+            if (meal == null)
+            {
+                return NotFound(new { message = "找無餐點資訊" });
+            }
+            return Ok(meal);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
