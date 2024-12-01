5e1b426b39e4a2e872c5ccfb616263a57eebb519
poyu <chen06115@gmail.com>
2024-12-01 18:26:18 +0800

    建立EF中間表關聯

--- web/senior_project_web/senior_project_web/Data/OrderSystemDbContext.cs
+++ web/senior_project_web/senior_project_web/Data/OrderSystemDbContext.cs
@@ -17,6 +17,7 @@ namespace senior_project_web.Data
         public DbSet<Order_MealModel> Order_Meal { get; set; }
         public DbSet<PredictionModel> Prediction { get; set; }
         public DbSet<Daily_Sales_ReportModel> Daily_Sales_Report { get; set; }
+        public DbSet<ReportMealModel> ReportMeal { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -33,6 +34,17 @@ namespace senior_project_web.Data
                 .HasOne(m => m.Inventory)
                 .WithMany(i => i.Meal)
                 .HasForeignKey(m => m.inventory_id);
+            modelBuilder.Entity<Daily_Sales_ReportModel>()
+                .HasMany(r => r.Meal)
+                .WithMany(m => m.Daily_Sales_Report)
+                .UsingEntity<ReportMealModel>(
+                    j => j.HasOne(rm => rm.Meal) // 中間表對 MealModel 的外鍵
+                          .WithMany(m => m.ReportMeal) // MealModel 中對應的集合
+                          .HasForeignKey(rm => rm.meal_id), // 外鍵欄位
+                    j => j.HasOne(rm => rm.Report) // 中間表對 Daily_Sales_ReportModel 的外鍵
+                          .WithMany(r => r.ReportMeal) // Daily_Sales_ReportModel 中對應的集合
+                          .HasForeignKey(rm => rm.report_id) // 外鍵欄位
+                );
         }
     }
 }
