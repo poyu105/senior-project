ec07aa2c3999ae2e433dabebfbf2b1fbb101d11b
poyu <chen06115@gmail.com>
2024-12-08 23:39:03 +0800

    新增購物車Modal，不會於資料庫建檔，僅用於顯示於網頁

---
+++ web/senior_project_web/senior_project_web/Models/CartModel.cs
@@ -0,0 +1,8 @@
+﻿namespace senior_project_web.Models
+{
+    //購物車Model，不會於資料庫中建檔，僅用於顯示於網頁
+    public class CartModel : Order_MealModel
+    {
+        public MealModel Meal { get; set; }
+    }
+}
