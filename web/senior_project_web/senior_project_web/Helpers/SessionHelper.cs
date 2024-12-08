9ab725f33514d3475725c8abd72d0221ab10aa78
poyu <chen06115@gmail.com>
2024-12-08 23:38:24 +0800

    新增Session功能，用來暫時儲存購物車資料

---
+++ web/senior_project_web/senior_project_web/Helpers/SessionHelper.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+
+namespace senior_project_web.Helpers
+{
+    public static class SessionHelper
+    {
+        //設定Session內容
+        public static void SetObjectAsJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        //取得Session內容
+        public static T GetObjectFromJson<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+
+        //移除 Session 
+        public static void Remove(this ISession session, string key)
+        {
+            session.Remove(key);
+        }
+    }
+}
