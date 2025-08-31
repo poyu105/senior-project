f446edffa7d179212673401583e26c4ae44b5c0a
poyu <chen06115@gmail.com>
2025-08-31 15:54:41 +0800

    feat(UserRegisterDto.cs):新增顧客註冊DtoModel

---
+++ new_web/orderSys/orderSys_bk/orderSys_bk/Model/Dto/UserRegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace orderSys_bk.Model.Dto
+{
+    public class UserRegisterDto
+    {
+        public string[] photos { get; set; } //照片陣列
+        public string username { get; set; } //使用者名稱
+        public string phone_number { get; set; } //電話號碼
+    }
+}
