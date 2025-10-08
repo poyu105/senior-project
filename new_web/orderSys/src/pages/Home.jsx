76441084b7338a9729b939022c777d33e9edec84
poyu <poyuwork06115@gmail.com>
2025-10-08 22:07:51 +0800

    feat(Home.jsx):新增全域BASE_URL設定、添加登出功能、添加取得用戶位置功能、介接推薦餐點API、顯示推薦餐點功能

--- new_web/orderSys/src/pages/Home.jsx
+++ new_web/orderSys/src/pages/Home.jsx
@@ -8,10 +8,12 @@ import Modal from "../components/Modal";
 import { useCart } from "../context/CartContext";
 import { useUser } from "../context/UserContext";
 
+const VITE_BASE_URL = import.meta.env.VITE_BASE_URL;
+
 export default function Home(){
     const { setLoading } = useLoading();
     const { addToCart } = useCart(); //購物車Context
-    const { user, login, register } = useUser(); //用戶Context
+    const { user, login, register, logout } = useUser(); //用戶Context
     
     const [meals, setMeals] = useState([]); //餐點列表
 
@@ -31,19 +33,68 @@ export default function Home(){
     const [registerInfo, setRegisterInfo] = useState({name: "", phone: ""}); //註冊資訊
     const [showRegisterCamera, setShowRegisterCamera] = useState(false); //顯示註冊相機
 
+    const [recommendMeals, setRecommendMeals] = useState([]); //推薦餐點
+
+    //取得使用者位置
+    const [location, setLocation] = useState({ latitude: 25.0478, longitude: 121.5319 });
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setLocation({
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    });
+                    console.log(position.coords.latitude, position.coords.longitude);
+                },
+                (error) => {
+                    console.error(error);
+                    //台北預設座標
+                    setLocation({ latitude: 25.0478, longitude: 121.5319 });
+                }
+            );
+        } else {
+            console.error("瀏覽器不支援 Geolocation API");
+        }
+    }, []);
+
     //取得餐點資訊
     useEffect(()=>{
-        const fetchData = async ()=>{
+        const fetchMeals = async ()=>{
             setLoading(true);
             try {
                 const res = await ApiServices.getMeals();
-                setMeals(res);
+                if(res.success){
+                    setMeals(res.meals);
+                }
             } catch (error) {
-                console.error(`發生錯誤: ${error}`);
+                console.error(`取得餐點資訊時發生錯誤: ${error}`);
             }
             setLoading(false);
         }
-        fetchData();
+        const fetchRecommendMeals = async ()=>{
+            try {
+                setLoading(true);
+                const date = new Date();
+                const year = date.getFullYear();
+                const month = String(date.getMonth() + 1).padStart(2, "0"); // 月份要 +1
+                const day = String(date.getDate()).padStart(2, "0");
+                const formatted = `${year}-${month}-${day}`;
+                const res = await ApiServices.getRecommendMeals({ 
+                    date: formatted, //當天日期
+                    location: location  // 使用者位置
+                });
+                if(res.success){
+                    setRecommendMeals(res.meals);
+                }   
+            } catch (error) {
+                console.error(`取得推薦餐點資訊時發生錯誤: ${error}`);
+            } finally{
+                setLoading(false);
+            }
+        }
+        fetchMeals();
+        fetchRecommendMeals();
     },[,user]);
 
     //監控加入購物車數量變化
@@ -143,33 +194,84 @@ export default function Home(){
                 {/* Sidebar */}
                 <div className="col-2 text-center">
                     <Sidebar selectType={selectType} setSelectType={setSelectType}/>
-                    {/* 登入Btn */}
-                    <button
-                        type="button"
-                        className="btn btn-warning w-100 mb-2"
-                        onClick={()=>{
-                            setShowLoginModal(true);
-                        }}>
-                        快速登入
-                    </button>
-                    {/* 註冊Btn */}
-                    <button
-                        type="button"
-                        className="btn btn-outline-secondary w-100 mt-2"
-                        onClick={()=>{
-                            setShowRegisterModal(true);
-                        }}>
-                        註冊
-                    </button>
+                    {
+                        user ? (
+                            <>
+                                <p className="mt-3">登入成功，請點餐!</p>
+                                {/* 登入Btn */}
+                                <button
+                                    type="button"
+                                    className="btn btn-outline-secondary w-100 mb-2"
+                                    onClick={()=>{
+                                        logout();
+                                    }}>
+                                    登出
+                                </button>
+                            </>
+
+                        ) : (
+                            <>
+                                {/* 登入Btn */}
+                                <button
+                                    type="button"
+                                    className="btn btn-warning w-100 mb-2"
+                                    onClick={()=>{
+                                        setShowLoginModal(true);
+                                    }}>
+                                    快速登入
+                                </button>
+                                {/* 註冊Btn */}
+                                <button
+                                    type="button"
+                                    className="btn btn-outline-secondary w-100 mt-2"
+                                    onClick={()=>{
+                                        setShowRegisterModal(true);
+                                    }}>
+                                    註冊
+                                </button>
+                            </>
+                        )
+                    }
                 </div>
                 {/* 餐點Card */}
                 <div className="mx-auto p-0 col-9">
-                    <div className="card-grid border border-2 border-warning rounded-4">
+                    <div className={`${selectType == "recommend" && recommendMeals.length == 0 ? "h-100" : "card-grid"} border border-2 border-warning rounded-4`}>
+                        {
+                            //推薦餐點
+                            selectType == "recommend" && (recommendMeals.length > 0 ? (
+                                recommendMeals.map((meal, index) => (
+                                    <Card
+                                        key={index}
+                                        showImg={true}
+                                        imgPath={`${VITE_BASE_URL}${meal.img_path}`}
+                                        title={meal.name}
+                                        onClickFunc={()=>{
+                                                setShowAddCartModal(true)
+                                                setMealInfo(meal);
+                                            }}>
+                                        <ul className="list-unstyled text-secondary" style={{fontSize: "14px"}}>
+                                            <li>餐點類型: {meal.type}</li>
+                                            <li>
+                                                餐點介紹:
+                                                <p>{meal.description}</p>
+                                            </li>
+                                        </ul>
+                                        <div className="d-flex flex-row justify-content-between align-items-center gap-2">
+                                            <p className="card-text fs-5 m-0">NT$ {meal.price}</p>
+                                            <button className="btn bg-light border">
+                                                <i className="bi bi-cart-plus"></i>
+                                            </button>
+                                        </div>
+                                    </Card>
+                            ))) : (
+                                <p className="fs-4 text-secondary text-center my-3">今日推薦餐點尚未推出，請選擇其他分類!</p>
+                            ))
+                        }
                         {meals.filter(meal => selectType == "all" ? meal : meal.type == selectType).map((meal, index) => (
                             <Card
                                 key={index}
                                 showImg={true}
-                                imgPath={`https://localhost:7220/${meal.img_path}`}
+                                imgPath={`${VITE_BASE_URL}${meal.img_path}`}
                                 title={meal.name}
                                 onClickFunc={()=>{
                                         setShowAddCartModal(true)
@@ -209,7 +311,7 @@ export default function Home(){
                 <div className="mx-auto border border-3 rounded" style={{width: "80%"}}>
                     <img
                         style={{width: "100%", height: "250px", objectFit: "cover"}}
-                        src={`https://localhost:7220/${mealInfo?.img_path}`}/>
+                        src={`${VITE_BASE_URL}${mealInfo?.img_path}`}/>
                     <div className="p-2">
                         <ul className="list-unstyled m-0">
                             <li className="fs-3">{mealInfo?.name}</li>
